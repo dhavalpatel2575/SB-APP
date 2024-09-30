5319edfe2fcd2a5f5b7314acef157217dbbee72b
DhavalPatel <jaydipa94patel@gmail.com>
2024-09-30 12:43:22 -0400

    upload to git

---
+++ src/main/java/com/example/AWS_EC2/AwsEc2Application.java
@@ -0,0 +1,20 @@
+package com.example.AWS_EC2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AwsEc2Application {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Hello";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEc2Application.class, args);
+	}
+
+}
